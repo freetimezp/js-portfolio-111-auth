0d6b7414b1cb1f6fc3823376e6342fa77712a961
freetimezp <kraftukrnet@ukr.net>
2024-08-11 19:18:41 +0300

    08. js-portfolio-111-auth. Logout endpoint.

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -105,7 +105,9 @@ export const login = async (req, res) => {
 };
 
 export const logout = async (req, res) => {
-    res.send("logout route");
+    res.clearCookie("token");
+
+    res.status(200).json({ success: true, message: "Logout success!" });
 };
 
 
