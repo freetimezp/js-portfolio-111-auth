c04ecd787196876dd30612fd8192b52fa903b4ad
freetimezp <kraftukrnet@ukr.net>
2024-08-13 21:30:38 +0300

    15. js-portfolio-111-auth. PasswordStrengthMeter check component.

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -4,6 +4,7 @@ import { motion } from "framer-motion";
 import { User, Mail, Lock } from "lucide-react";
 
 import Input from "../components/Input";
+import PasswordStrengthMeter from "../components/PasswordStrengthMeter";
 
 const SignupPage = () => {
     const [name, setName] = useState('');
@@ -52,6 +53,8 @@ const SignupPage = () => {
                     />
 
                     {/* password strength meter */}
+                    <PasswordStrengthMeter password={password} />
+
                     <motion.button
                         className="mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 
                         to-emerald-600 text-white font-bold rounded-lg shadow-lg hover:from-green-600 
